f14ec9dba81557b1741f8d4f93c6db7afcf77b26
nezam0003 <fhameedk.nezam@gmail.com>
2021-09-24 00:11:30 +0600

    default error handler setup

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
+/*********** Dependencies **********/
 const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 
 const app = express();
+dotenv.config();
 
-app.listen(8000, () => console.log("server running on port 8000"));
+/******* Connect to Database ********/
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection successfull"))
+  .catch((err) => console.log(err));
+
+/******* Request Parsers ********/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/********** Set view engine **********/
+app.set("view engine", "ejs");
+
+/******* set static folder ********/
+app.use(express.static(path.join(__dirname, "public")));
+
+/******* Parse cookies ********/
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+/********** Router setup *********/
+
+/************ 404 not found Handler ***********/
+app.use(notFoundHandler);
+
+/************ common error Handler ***********/
+app.use(errorHandler);
+
+/************ server start ************/
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server Running On Port ${PORT}`));
